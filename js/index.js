daa0751de104c5f6053c36f5d04ae7a07174fb60
Tom Fleischer <tfleischer@gmail.com>
2013-08-02 00:40:27 -0400

    Added PR interval (no UI)

--- js/index.js
+++ js/index.js
@@ -1,30 +1,44 @@
 rate = 0;
 interval = null;
+pr_interval = 1.0; // for purposes of this app, 'normal' is 1.0
 
 function beat($la, $ra, $lv, $rv) {
-  inRate = parseInt(200 * (rate / 800));
-  outRate = parseInt(400 * (rate / 800));
+  var laInRate        = parseInt(200 * (rate / 800));
+  var laOutRate       = parseInt(400 * (rate / 800));
+  var laOutRatePR     = parseInt(laOutRate * pr_interval);
+  var laOutRatePRDiff = laOutRatePR - laOutRate;
 
-  lvTimeout = parseInt(75 * (rate / 800));
-  raTimeout = parseInt(150 * (rate / 800));
-  rvTimeout = parseInt(225 * (rate / 800));
+  var lvInRate  = parseInt(200 * (rate / 800));
+  var lvOutRate = parseInt(400 * (rate / 800));
 
-  $la.animate({backgroundColor: "#82416D"}, inRate);
-  $la.animate({backgroundColor: "#999"}, outRate);
+  var raInRate        = parseInt(200 * (rate / 800));
+  var raOutRate       = parseInt(400 * (rate / 800));
+  var raOutRatePR     = parseInt(raOutRate * pr_interval);
+  var raOutRatePRDiff = raOutRatePR - raOutRate;
+
+  var rvInRate  = parseInt(200 * (rate / 800));
+  var rvOutRate = parseInt(400 * (rate / 800));
+
+  var lvTimeout = parseInt(75 * (rate / 800) + laOutRatePRDiff);
+  var raTimeout = parseInt(150 * (rate / 800) + laOutRatePRDiff);
+  var rvTimeout = parseInt(225 * (rate / 800) + laOutRatePRDiff + raOutRatePRDiff);
+
+  $la.animate({backgroundColor: "#82416D"}, laInRate);
+  $la.animate({backgroundColor: "#999"}, laOutRatePR);
 
   setTimeout(function() {
-    $lv.animate({backgroundColor: "#82416D"}, inRate);
-    $lv.animate({backgroundColor: "#999"}, outRate);
+    $lv.animate({backgroundColor: "#82416D"}, lvInRate);
+    $lv.animate({backgroundColor: "#999"}, lvOutRate);
   }, lvTimeout);
 
   setTimeout(function() {
-    $ra.animate({backgroundColor: "#C93030"}, inRate);
-    $ra.animate({backgroundColor: "#999"}, outRate);
+    $ra.animate({backgroundColor: "#C93030"}, raInRate);
+    $ra.animate({backgroundColor: "#999"}, raOutRatePR);
   }, raTimeout);
 
   setTimeout(function() {
-    $rv.animate({backgroundColor: "#C93030"}, inRate);
-    $rv.animate({backgroundColor: "#999"}, outRate);
+    $rv.animate({backgroundColor: "#C93030"}, rvInRate);
+    $rv.animate({backgroundColor: "#999"}, rvOutRate);
   }, rvTimeout);
 }
 
